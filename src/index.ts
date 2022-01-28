5511933a9982a6ba11d683b71cc88f80ea23f38f
SieR-VR <nwh63@naver.com>
2022-01-29 07:28:33 +0900

    Monochrome implementation

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,39 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import EventEmitter from 'events';
-
 import md5 from 'md5';
+
+import sharp from 'sharp';
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
 import videoshow from 'videoshow';
-import jpeg from 'jpeg-js';
 
-import { toUTF8Array, rgbToRgba, getWH } from './utils';
+import { toUTF8Array, bitsToSVG } from './utils';
 
-export default async function Prism(input: string): Promise<Buffer> {
+const MAX_SIZE = 1280 * 720;
+
+export default async function Prism(input: string) {
     const outputPath = path.join(__dirname, md5(input));
 
     const utf8array = toUTF8Array(input);
     const input_bytes = Uint32Array.from(utf8array).buffer;
     const input_uint8 = new Uint8Array(input_bytes);
 
-    const input_processed = rgbToRgba(input_uint8);
-    const size = input_processed.byteLength / 4;
+    if (input_uint8.byteLength * 8 > MAX_SIZE) {
+        throw new Error('Input is too large');
+    }
 
-    const { width, height } = getWH(size);
+    const input_processed = bitsToSVG(input_uint8);
+    await sharp(Buffer.from(input_processed)).jpeg().toFile(outputPath + '.jpg');
 
-    const jpeg_data = jpeg.encode({ data: input_processed, width, height }, 100);
-
-    fs.writeFileSync(outputPath + '.jpg', jpeg_data.data);
+    videoshow.ffmpeg.setFfmpegPath(ffmpegPath);
     const vs: EventEmitter = videoshow([outputPath + '.jpg'], {
         fps: 1,
+        loop: 5,
         transition: false,
         videoBitrate: 10000,
         videoCodec: 'libx264',
         format: 'mp4',
-        pixelFormat: 'yuv420p'
+        pixelFormat: 'yuv420p',
     }).save(outputPath + '.mp4');
 
     return new Promise((resolve, reject) => {
@@ -38,7 +42,7 @@ export default async function Prism(input: string): Promise<Buffer> {
         vs.on('end', () => {
             const buffer = fs.readFileSync(outputPath + '.mp4');
             fs.unlinkSync(outputPath + '.jpg');
-            fs.unlinkSync(outputPath + 'mp4');
+            fs.unlinkSync(outputPath + '.mp4');
             resolve(buffer);
         });
     });
--- src/utils.ts
+++ src/utils.ts
@@ -8,19 +8,35 @@ export function toUTF8Array(str: string) {
 }
 
 export function rgbToRgba(pre_buffer: Uint8Array) {
+    const MAX_SIZE = 1280 * 720;
+
     let buffer: Buffer;
 
     if (pre_buffer.byteLength % 3) buffer = Buffer.concat([pre_buffer, Buffer.alloc(3 - pre_buffer.byteLength % 3, 0)]);
     else buffer = Buffer.from(pre_buffer);
 
-    const rgba = new Uint8Array(buffer.length / 3 * 4);
+    let rgba = new Uint8Array(buffer.length / 3 * 4);
     for (let i = 0; i < buffer.length; i++) {
         rgba[i * 4] = buffer[i * 3];
         rgba[i * 4 + 1] = buffer[i * 3 + 1];
         rgba[i * 4 + 2] = buffer[i * 3 + 2];
         rgba[i * 4 + 3] = 255;
     }
+
+    rgba = Buffer.concat([Buffer.from(rgba), Buffer.alloc(MAX_SIZE - rgba.length, 0)]);
     return rgba;
+}
+
+export function bitsToSVG(bits: Uint8Array) {
+    let res = "<svg xmlns='http://www.w3.org/2000/svg' width='1280' height='720' viewBox='0 0 1280 720'>";
+
+    for (let i = 0; i < bits.byteLength * 8; i++) {
+        if (bits[Math.floor(i / 8)] & (1 << (i % 8)))
+            res += `<rect x='${(i * 10) % 1280}' y='${Math.floor((i * 10) / 1280) * 10}' width='10' height='10' fill='white' />`;
+    }
+
+    res += "</svg>";
+    return res;
 }
 
 export function getWH(size: number): { width: number, height: number } {
